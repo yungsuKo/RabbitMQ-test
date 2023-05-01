145f0db2717a4fe5710659f0cfd91f0909e93740
Tom Ko <yungsu2391@gmail.com>
2023-05-01 17:29:33 +0900

    [ ADD ] rabbitmq basic

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { RabbitmqService } from './apis/rabbitmq/rabbitmq.service';
 import { ProductsModule } from './apis/products/products.module';
 import { RabbitmqController } from './apis/rabbitmq/rabbitmq.controller';
+import { RabbitmqModule } from './apis/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, RabbitmqModule],
   controllers: [AppController, RabbitmqController],
   providers: [AppService, RabbitmqService],
 })
